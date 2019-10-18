99d29717c05d0faa5e4560293ed70e00a8383f93
amani <abo@mobilepowered.fr>
2019-10-18 15:11:46 +0100

    modification effects

--- src/app/shared/effects/effects.ts
+++ src/app/shared/effects/effects.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
- import { createEffect, ofType, Actions } from '@ngrx/effects';
+import { createEffect, ofType, Actions } from '@ngrx/effects';
 import { map, mergeMap, catchError } from 'rxjs/operators';
 import { EMPTY } from 'rxjs';
+import * as Action from './../action/action';
 
 
 @Injectable()
   export class WeatherEffect {
     
     loadWeather$ = createEffect(() => { console.log('effect'); return this.actions$.pipe(
-      ofType('[WEATHER] GET'),
+      ofType(Action.GET_WEATHER),
       mergeMap((action : any) => this.apiService.getWeatherData(action.payload)
         .pipe(
-          map(weather => ({ type: '[WEATHER] DATA', payload: weather })),
+          map(weather => ({ type: Action.LOADED_WEATHER, payload: weather })),
           catchError(() => EMPTY)
         ))
     )});
