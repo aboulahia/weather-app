0209f815778ecc3512fa8a9595cc8e5f1affbcc8
amani <abo@mobilepowered.fr>
2019-10-19 12:15:03 +0100

    add gard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent , canActivate:[AuthGuard]},
   {path: '**', redirectTo: 'login'}
 ];
 
